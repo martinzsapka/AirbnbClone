efc1a9faa0e702fa213d830e7a047521e68a7fa7
Martin Zsapka <martin.zsapka@gmail.com>
2025-06-08 11:12:35 +0200

    ExploreView

---
+++ AirbnbClone/Core/Explore/ExploreView.swift
@@ -0,0 +1,29 @@
+//
+//  ExploreView.swift
+//  SwiftUIDemo
+//
+//  Created by Support Aximmetry on 07/06/2025.
+//
+
+import SwiftUI
+
+struct ExploreView: View {
+    var body: some View {
+        NavigationStack {
+            ScrollView {
+                LazyVStack(spacing: 32) {
+                    ForEach(0 ... 10, id: \.self) { listing in
+                        ListingItemView()
+                            .frame(height: 400)
+                            .clipShape(RoundedRectangle(cornerRadius: 10))
+                    }
+                }
+                .padding()
+            }
+        }
+    }
+}
+
+#Preview {
+    ExploreView()
+}
---
+++ AirbnbClone/Core/Listings/ListingItemView.swift
@@ -0,0 +1,63 @@
+//
+//  ListingItemView.swift
+//  SwiftUIDemo
+//
+//  Created by Support Aximmetry on 07/06/2025.
+//
+
+import SwiftUI
+
+struct ListingItemView: View {
+    
+    var images = [
+        "listing-1",
+        "listing-2",
+        "listing-3",
+        "listing-4"
+    ]
+    
+    var body: some View {
+        VStack(spacing: 8) {
+            TabView {
+                ForEach(images, id: \.self) { image in
+                    Image(image)
+                }
+            }
+            .frame(height: 320)
+            .clipShape(RoundedRectangle(cornerRadius: 10))
+            .tabViewStyle(.page)
+            
+            HStack(alignment: .top) {
+                VStack(alignment: .leading) {
+                    Text("Miami, Florida")
+                        .fontWeight(.semibold)
+                    
+                    Text("12 mi away")
+                        .foregroundStyle(.gray)
+                    
+                    Text("Nov 3 - 10")
+                        .foregroundStyle(.gray)
+                    
+                    HStack(spacing: 4) {
+                        Text("$567")
+                            .fontWeight(.semibold)
+                        Text("night")
+                    }
+                }
+                
+                Spacer()
+                
+                HStack(spacing:2) {
+                    Image(systemName: "star.fill")
+                    
+                    Text("4.86")
+                }
+            }
+            .font(.footnote)
+        }
+    }
+}
+
+#Preview {
+    ListingItemView()
+}
