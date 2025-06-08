ba9b6d12cc4fd1e379e165f186ef613a0ae7ac8e
Martin Zsapka <martin.zsapka@gmail.com>
2025-06-08 10:58:49 +0200

    Project init

---
+++ AirbnbClone/AirbnbCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  AirbnbCloneApp.swift
+//  AirbnbClone
+//
+//  Created by Support Aximmetry on 08/06/2025.
+//
+
+import SwiftUI
+
+@main
+struct AirbnbCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
